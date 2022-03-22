7b7464ac64550ff46a55e5a2f9856277e3bc350a
Harrison Browning <moochiejinks@Harrisons-MacBook-Pro.local>
2022-03-22 11:13:23 -0500

    completed assignment 1

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,11 +8,12 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
+let favFood = ['Wings', 'Thai Curry', 'Grilled Cheese', 'Talapia', 'Chicken Ceasar Wrap'];
 
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
-
+console.log('While I will eat almost anything, my favorite dishes are: ', favFood);
 
 
 // 2. Array.length - tells you how many items are in the array
@@ -22,6 +23,7 @@ console.log('--- 2. Length of an array ---');
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
+console.log('Number of favorite foods: ', favFood.length);
 
 
 // 3. Accessing array items
@@ -31,14 +33,16 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array 
+console.log('The second animal is: ', animalArray[1]);
 
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
+console.log('The last animal in our list is: ', animalArray[animalArray.length - 1]);
 
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
-
+console.log('The last animal in our list is: ', animalArray[animalArray.length - 1]);
 
 
 // 4. Adding & Removing Array Items
@@ -49,7 +53,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
+favFood.push('Shawarma')
+console.log('Another food I love is', favFood[favFood.length - 1], '!'); console.log('This makes the list of my favorite foods: ', favFood);
 
 
 // Example: Remove the last animal by using Array.pop
@@ -59,13 +64,17 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
 //      log both the food removed and the updated array
-
+let removedFood = favFood.pop();
+console.log('I removed the last food item', removedFood);
+console.log('The list of my favorite foods are now:', favFood);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
+favFood.unshift('Fajitas');
+console.log('I added a new favorite food to the beginning of the array', favFood);
 
 
 // Example: Remove the first animal using Array.shift
@@ -75,20 +84,31 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array & 
 //     log both the food removed and the updated array
+removedFood = favFood.shift();
+console.log('Removed the first food item', removedFood);
+console.log('My new list of favorite foods are', favFood);
 
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
+favFood[1] = 'Burgers';
+console.log(favFood);
+
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
+favFood = favFood.sort().reverse();
+console.log(favFood);
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+console.log(favFood.join(' and '));
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+const foodAnimals = favFood.concat(animalArray);
+console.log(foodAnimals);
